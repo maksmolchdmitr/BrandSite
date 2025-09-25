4c4851d6493dc7e47517b56537b9f322b4a8bc43
maksmolch <maksmolchdmitr@gmail.com>
2025-09-25 20:51:16 +0300

    change description text

--- brand-site/src/components/Decsription.vue
+++ brand-site/src/components/Decsription.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>Hello, I’m <a className="refText">Maks.</a> <br> I work as Backend Software Engineer and have studied at HSE.</p>
+  <p>Hello, I’m <a className="refText">Maks</a> <br> I work as Backend Software Engineer and have studied at HSE</p>
 </template>
 
 <script>
